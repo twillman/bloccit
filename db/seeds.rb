6dc7a6dd15090d4c1ce4f968d2ad3bfe1906a861
Tara Willman <tara.willman@gmail.com>
2016-05-29 23:25:59 -0700

    Assignment 30 completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,10 @@
 require 'random_data'
 
 #Create Posts
+unique_post = Post.find_or_create_by(
+  title:  "Unique Post",
+  body:   "This is the unique post."
+)
 50.times do
   Post.create!(
     title:  RandomData.random_sentence,
@@ -10,6 +14,10 @@ end
 posts = Post.all
 
 #Create Comments
+Comment.find_or_create_by(
+  post:   unique_post,
+  body:   "This is the unique comment."
+)
 100.times do
   Comment.create!(
     post:   posts.sample,
