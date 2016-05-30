1263710c20f64131a796e069c28dc4920599e150
Tara Willman <tara.willman@gmail.com>
2016-05-30 02:23:06 -0700

    Assignment 32 completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,31 @@
 require 'random_data'
 
 #Create Posts
-50.times do
-  Post.create!(
-    title:  RandomData.random_sentence,
-    body:   RandomData.random_paragraph
-  )
-end
-posts = Post.all
+#50.times do
+#  Post.create!(
+#    title:  RandomData.random_sentence,
+#    body:   RandomData.random_paragraph
+#  )
+#end
+#posts = Post.all
 
 #Create Comments
-100.times do
-  Comment.create!(
-    post:   posts.sample,
-    body:   RandomData.random_paragraph
+#100.times do
+#  Comment.create!(
+#    post:   posts.sample,
+#    body:   RandomData.random_paragraph
+#  )
+#end
+
+50.times do
+  Advertisement.create!(
+  title: RandomData.random_sentence,
+  body: RandomData.random_paragraph,
+  price: rand(1..1000)
   )
 end
 
 puts "Seed finished"
-puts "#{Post.count} posts created"
-puts "#{Comment.count} comments created"
+#puts "#{Post.count} posts created"
+#puts "#{Comment.count} comments created"
+puts "#{Advertisement.count} advertisements created"
