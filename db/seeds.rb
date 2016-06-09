819a97f79409d725a077f733ced89a47d6aec1f9
Tara Willman <tara.willman@gmail.com>
2016-06-09 07:28:35 -0700

    Checkpoint 39 Posts and Users: completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,30 @@
 require 'random_data'
+#Create Users
+5.times do
+  User.create!(
+  name:     RandomData.random_name,
+  email:    RandomData.random_email,
+  password: RandomData.random_sentence
+  )
+end
+users = User.all
+
 #Create topics
 15.times do
   Topic.create!(
-  name:   RandomData.random_sentence,
-  description: RandomData.random_paragraph
+  name:         RandomData.random_sentence,
+  description:  RandomData.random_paragraph
   )
 end
 topics = Topic.all
+
 #Create Posts
 50.times do
   Post.create!(
-    title:  RandomData.random_sentence,
-    body:   RandomData.random_paragraph
+  user:   users.sample,
+  topic:  topics.sample,
+  title:  RandomData.random_sentence,
+  body:   RandomData.random_paragraph
   )
 end
 posts = Post.all
@@ -19,12 +32,19 @@ posts = Post.all
 #Create Comments
 100.times do
   Comment.create!(
-    post:   posts.sample,
-    body:   RandomData.random_paragraph
+  post:   posts.sample,
+  body:   RandomData.random_paragraph
   )
 end
 
+user = User.first
+user.update_attributes!(
+  email: 'tara.willman@gmail.com',
+  password: 'helloworld'
+)
+
 puts "Seed finished"
-puts"#{Topic.count} topics created"
+puts "#{User.count} users created"
+puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
