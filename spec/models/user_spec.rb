452870b49850c96394864a761880f384e4cdd39f
Tara Willman <tara.willman@gmail.com>
2016-06-09 09:50:25 -0700

    Checkpoint 40 Authorization: completed

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -34,5 +34,43 @@ RSpec.describe User, type: :model do
     it "should be an invalid user due to blank email" do
       expect(user_with_invalid_email).to_not be_valid
     end
+
+    it "responds to role" do
+      expect(user).to respond_to(:role)
+    end
+
+    it "responds to admin?" do
+      expect(user).to respond_to(:admin?)
+    end
+
+    it "responds to member?" do
+      expect(user).to respond_to(:member?)
+    end
+  end
+
+  describe "roles" do
+    it "is member by default" do
+      expect(user.role).to eq("member")
+    end
+
+    context "member user" do
+      it "returns true for #member?" do
+        expect(user.member?).to be_truthy
+      end
+
+      it "returns false for#admin?" do
+        expect(user.admin?).to be_falsey
+      end
+    end
+
+    context "admin user" do
+      before do
+        user.admin!
+      end
+
+      it "returns false for #member?" do
+        expect(user.member?).to be_falsey
+      end
+    end
   end
 end
