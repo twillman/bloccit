95e93a6ffb276925a0e1b6a1c34c4e70998c69d9
Tara Willman <tara.willman@gmail.com>
2016-06-14 16:45:13 -0700

    Checkpoint 43 voting: completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   resources :labels, only: [:show]
-  
+
   resources :topics do
     resources :posts, except: [:index]
   end
   resources :posts, only: [] do
     resources :comments, only: [:create, :destroy]
+    post '/up-vote' => 'votes#up_vote', as: :up_vote
+    post '/down-vote' => 'votes#down_vote', as: :down_vote
   end
 
   resources :users, only: [:new, :create]
