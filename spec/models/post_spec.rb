fd7e0a15c1546f219b5b0f8778d69de1afbbafef
Tara Willman <tara.willman@gmail.com>
2016-06-16 07:42:16 -0700

    Checkpoint 45 public profiles: completed

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -6,11 +6,11 @@ RSpec.describe Post, type: :model do
   let(:title) {RandomData.random_sentence}
   let(:body) {RandomData.random_paragraph}
 
-  let(:topic) {Topic.create!(name: name, description: description)}
+  let(:topic) {create(:topic)}
 
-  let(:user) {User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "helloworld")}
+  let(:user) {create(:user)}
 
-  let(:post) {topic.posts.create!(title: title, body: body, user: user)}
+  let(:post) {create(:post)}
 
   it {is_expected.to have_many(:labelings)}
   it { is_expected.to have_many(:labels).through(:labelings)}
@@ -31,8 +31,8 @@ RSpec.describe Post, type: :model do
   it {is_expected.to validate_length_of(:body).is_at_least(20)}
 
   describe "attributes" do
-    it "has title, body, and user attribute" do
-      expect(post).to have_attributes(title: title, body: body, user: user)
+    it "has title, body attribute" do
+      expect(post).to have_attributes(title: post.title, body: post.body)
     end
   end
 
